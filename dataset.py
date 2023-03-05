ca60ab212a6fd2a862221bfd6106d43b83762b64
ting0602 <ting0602.cs09@ncyu.edu.tw>
2023-03-05 14:14:28 +0800

    feat: init

---
+++ dataset.py
@@ -0,0 +1,48 @@
+import os
+import torch
+from torchvision import datasets, transforms
+import config as c
+from torchvision.datasets import ImageFolder
+from torch.utils.data import Dataset
+import numpy as np
+
+def load_datasets(dataset_path, class_name):
+    '''
+    Expected folder/file format to find anomalies of class <class_name> from dataset location <dataset_path>:
+
+    train data:
+
+            dataset_path/class_name/train/good/any_filename.png
+            dataset_path/class_name/train/good/another_filename.tif
+            dataset_path/class_name/train/good/xyz.png
+            [...]
+
+    test data:
+
+        'normal data' = non-anomalies
+
+            dataset_path/class_name/test/good/name_the_file_as_you_like_as_long_as_there_is_an_image_extension.webp
+            dataset_path/class_name/test/good/did_you_know_the_image_extension_webp?.png
+            dataset_path/class_name/test/good/did_you_know_that_filenames_may_contain_question_marks????.png
+            dataset_path/class_name/test/good/dont_know_how_it_is_with_windows.png
+            dataset_path/class_name/test/good/just_dont_use_windows_for_this.png
+            [...]
+
+        anomalies - assume there are anomaly classes 'crack' and 'curved'
+
+            dataset_path/class_name/test/crack/dat_crack_damn.png
+            dataset_path/class_name/test/crack/let_it_crack.png
+            dataset_path/class_name/test/crack/writing_docs_is_fun.png
+            [...]
+
+            dataset_path/class_name/test/curved/wont_make_a_difference_if_you_put_all_anomalies_in_one_class.png
+            dataset_path/class_name/test/curved/but_this_code_is_practicable_for_the_mvtec_dataset.png
+            [...]
+    '''
+
+def make_dataloaders(trainset, testset):
+    trainloader = torch.utils.data.DataLoader(trainset, pin_memory=True, batch_size=c.batch_size, shuffle=True,
+                                              drop_last=False)
+    testloader = torch.utils.data.DataLoader(testset, pin_memory=True, batch_size=c.batch_size, shuffle=False,
+                                             drop_last=False)
+    return trainloader, testloader
